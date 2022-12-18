bf6b3edf91ec6203e4bb93b9c1264d27a8495914
Saurabh Tiwari <86095513+SaurabhTiwari4093@users.noreply.github.com>
2022-12-18 23:59:56 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,136 @@
+import { AppBar, Container, Typography, Card, TextField, Button, Box, Backdrop, CircularProgress } from '@mui/material';
+import React, { useState } from 'react';
+import * as XLSX from 'xlsx';
+import swal from 'sweetalert';
+
+export default function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [subject, setSubject] = useState('');
+  const [bodyHTML, setBodyHTML] = useState('');
+  const [fileName, setFileName] = useState('Header of email column must be Email and name column must be Name in excel sheet');
+  const [jsonData, setJsonData] = useState('');
+  const [backdropOpen, setBackdropOpen] = useState(false);
+  const [sendResponse, setSendResponse] = useState('Please wait');
+
+  const sandMail = async (e) => {
+    e.preventDefault();
+    setBackdropOpen(true);
+    try {
+      for (var i = 0; i < jsonData.length;) {
+        const receiver = jsonData[i];
+        const bodyHTMLWithNameReplaced = bodyHTML.replaceAll("{Name}", receiver.Name);
+        const bodyHTMLWithBreakTagAdded = bodyHTMLWithNameReplaced.replaceAll("\n", "<br>");
+        const formData = {
+          senderEmail: email,
+          appPassword: password,
+          subject: subject,
+          bodyHTML: bodyHTMLWithBreakTagAdded,
+          receiverName: receiver.Name,
+          receiverEmail: receiver.Email,
+        }
+        const requestOptions = {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData)
+        }
+        const url = `http://localhost:3000/api/sendMail`;
+        await fetch(url, requestOptions)
+          .then((response) => response.json())
+          .then((data) => {
+            if (data.status === 200) {
+              setSendResponse(i + 1 + " : " + data.message);
+              i++;
+            }
+            else {
+              swal({
+                title: "Some error occured",
+                text: data.message,
+                icon: "error",
+              }).then(() => {
+                setBackdropOpen(false);
+                setSendResponse('Please wait');
+                return
+              });
+            }
+          })
+      }
+      swal({
+        title: "All mail sent successfully",
+        icon: "success",
+      }).then(() => {
+        setBackdropOpen(false)
+        setSendResponse('Please wait');
+      })
+    }
+    catch (error) {
+      swal({
+        title: "Some error occured",
+        text: error,
+        icon: "error",
+      }).then(() => {
+        setBackdropOpen(false);
+        setSendResponse('Please wait');
+        return
+      });
+    }
+  }
+
+  const handleUpload = (e) => {
+    e.preventDefault();
+    var file = e.target.files[0];
+    e.target.value = null;
+    setFileName(file.name);
+    var reader = new FileReader();
+    reader.onload = function (e) {
+      var data = e.target.result;
+      let readedData = XLSX.read(data, { type: 'binary' });
+      const wsname = readedData.SheetNames[0];
+      const ws = readedData.Sheets[wsname];
+      const dataParse = XLSX.utils.sheet_to_json(ws, { header: 0 });
+      setJsonData(dataParse);
+    };
+    reader.readAsBinaryString(file);
+  }
+
+  return (
+    <>
+      <Backdrop
+        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={backdropOpen}
+      >
+        <Box>
+          <Box sx={{ justifyContent: "center", display: "flex" }}>
+            <CircularProgress color="inherit" />
+          </Box>
+          <Typography sx={{ mt: 2 }}>{sendResponse}</Typography>
+        </Box>
+      </Backdrop>
+      <AppBar position='static' sx={{ p: 2 }}>
+        <Typography variant="h6">
+          MailHero
+        </Typography>
+      </AppBar>
+      <Container maxWidth="md" sx={{ py: 2 }}>
+        <form onSubmit={sandMail}>
+          <Card sx={{ p: 2 }}>
+            <TextField label="Your gamil address" type="email" placeholder='xyz@gmail.com' fullWidth required sx={{ mb: 2 }} value={email} onChange={(e) => setEmail(e.target.value)} />
+            <TextField label="App password" placeholder='Google Account >> Security >> App Passwords >> Generate' fullWidth required sx={{ mb: 2 }} value={password} onChange={(e) => setPassword(e.target.value)} />
+            <TextField label="Subject of email" placeholder='Be clear and specific about the topic of the email' fullWidth required sx={{ mb: 2 }} value={subject} onChange={(e) => setSubject(e.target.value)} />
+            <TextField label="Body of email in html format" placeholder='In place of name of receiver write {Name} . Example: Dear Ram, => Dear {Name},' fullWidth required multiline minRows={8} sx={{ mb: 2 }} value={bodyHTML} onChange={(e) => setBodyHTML(e.target.value)} />
+            <Box sx={{ display: 'flex', alignItems: 'center', mb: 2 }}>
+              <TextField label="Excel file with email and name of receiver" value={fileName} fullWidth required />
+              <Button variant="outlined" component="label" sx={{ ml: 1, height: 54, textAlign: "center", width: 150 }}>
+                Upload Excel
+                <input type="file" onChange={(e) => handleUpload(e)} accept="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" hidden />
+              </Button>
+            </Box>
+            <Button variant="contained" type="submit" size="large">Send mail</Button>
+          </Card>
+        </form>
+      </Container>
+    </>
+  )
+}
